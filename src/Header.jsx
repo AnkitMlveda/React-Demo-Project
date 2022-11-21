4d48c63e59698a8006ab8ffcaa7232497ba0d0cb
ankitranpara <ankitranpara82@gmail.com>
2022-11-21 17:13:36 +0530

    Changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { NavLink,Navigate } from "react-router-dom";
 import "./Custom.css";
 import { useLocation } from "react-router-dom";
 import { useContext } from "react";
 import { Usercontext } from "./Usercontext";
+import Gravatar from "react-gravatar";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default function Header() {
   const location = useLocation().pathname;
@@ -10,12 +13,25 @@ export default function Header() {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <NavLink className="navbar-brand" to="/">
-          Movies
-        </NavLink>
-        {user && <p className="navbar-brand">Welcome, {user.user.name}</p>}
-      </nav>
+      <ErrorBoundary fallback={<Navigate to="/" />}>
+        <Suspense fallback={<p>loding....</p>}>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <NavLink className="navbar-brand" to="/">
+              Movies
+            </NavLink>
+            {/* {user && <p className="navbar-brand">Welcome, {user.user.name}
+        <NavLink className="nav-link custom1" to="/">
+              Logout
+        </NavLink></p>} */}
+            <Gravatar
+              style={{ width: "2rem", height: "2rem" }}
+              title={user?.user.name}
+              email={user?.user.email}
+              className="logintext"
+            />
+          </nav>
+        </Suspense>
+      </ErrorBoundary>
       {location === "/" ? (
         <div>
           <h1>Welcome to The site</h1>
--- src/MovieDetails.jsx
+++ src/MovieDetails.jsx
@@ -1,21 +1,33 @@
 export default function MovieDetails({ movies }) {
   return (
     <div className="row">
-      { movies.map( (movie)=> 
-      <div className="col-sm-3" key={movie._id}>
-        <div className="card-deck">
-          <div className="card">
-            <img src={movie.poster} alt={movie.title} className="card-img-top"/>
-            <div className="card-body">
-              <h5 className="card-title">{movie.title}</h5>
-              <p className="card-text">
-                {movie.fullplot}
-              </p>
+      {movies.map((movie) => (
+        <div className="col-sm-3" key={movie._id}>
+          <div className="card-deck">
+            <div className="card">
+              <img
+                src={movie.poster}
+                alt={movie.title}
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <p className="card-text">
+                  <h6>Cast:</h6>
+                  {movie.cast}
+                </p>
+                <p className="card-title">
+                  <h6>Title: </h6>
+                  {movie.title}
+                </p>
+                <p className="card-text">
+                  <h6>Description:</h6>
+                  {movie.fullplot}
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        </div>
-        )}
-      </div>
+      ))}
+    </div>
   );
 }
